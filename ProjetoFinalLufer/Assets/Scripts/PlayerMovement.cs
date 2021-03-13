130e86d6382ac77dc0929f2569afcd79e5d85aa4
mlhomsi <mlhomsi@gmail.com>
2021-03-13 00:38:50 -0300

    Ceninha

---
+++ ProjetoFinalLufer/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 6f;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+    public float dashSpeed;
+    public float dashTime;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if(direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            controller.Move(direction * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKeyDown("space"))
+        {
+            StartCoroutine(DashAction(direction));
+        }
+
+    }
+
+    IEnumerator DashAction(Vector3 dashDirection)
+    {
+        float startTime = Time.time;
+
+        while(Time.time < startTime + dashTime)
+        {
+            controller.Move(dashDirection * speed * Time.deltaTime);
+            yield return null;
+        }
+
+    }
+}
