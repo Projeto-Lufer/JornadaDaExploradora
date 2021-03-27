78529e62a083e557c9b12cd09ccaf87d3a3d3c9e
Bernardo <behornerl@gmail.com>
2021-03-27 14:27:20 -0300

    Criando script que rege o comportamento de objetos que podem ser levantados

---
+++ ProjetoFinalLufer/Assets/Scripts/Object interactivity/LiftableObject.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiftableObject : MonoBehaviour, Interactive
+{
+    [SerializeField] private Collider collider;
+    [SerializeField] private Rigidbody rigidbody;
+
+    [SerializeField] private float throwingVelocity;
+
+    public GameObject Interact(GameObject interactor)
+    {
+        return gameObject;
+    }
+
+    public void Lift(Transform liftedPosition)
+    {
+        SetColliderAndGravityEnabled(false);
+        GoToLiftedPosition(liftedPosition);
+    }
+
+    private void Release()
+    { 
+        transform.parent = null;
+        SetColliderAndGravityEnabled(true);
+    }
+
+    public void Drop(Vector3 droppingPosition)
+    {
+        Release();
+        transform.position = droppingPosition;
+    }
+
+    public void Throw(Vector3 throwingDirection)
+    {
+        Release();
+        rigidbody.velocity = throwingDirection * throwingVelocity;
+    }
+
+    private void SetColliderAndGravityEnabled(bool enabled)
+    {
+        collider.enabled = enabled;
+        rigidbody.useGravity = enabled;
+    }
+
+    private void GoToLiftedPosition(Transform liftedPosition)
+    {
+        //transform.position = liftedPosition.position;
+        transform.SetPositionAndRotation(liftedPosition.position, liftedPosition.rotation);
+        transform.parent = liftedPosition;
+    }
+}
