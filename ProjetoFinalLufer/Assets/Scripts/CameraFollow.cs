f1e9d286825cf3e8590cd6cd9724da95872d377e
Bernardo <behornerl@gmail.com>
2021-03-29 23:10:18 -0300

    Criando script para a câmera normal seguir o jogador (sem usar cinemachine)

---
+++ ProjetoFinalLufer/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform followTarget;
+    private Vector3 offset;
+
+    private void Start()
+    {
+        offset = new Vector3 (followTarget.position.x - transform.position.x, 
+                                followTarget.position.y - transform.position.y, 
+                                followTarget.position.z - transform.position.z);
+    }
+
+    void Update()
+    {
+        transform.position = followTarget.position - offset;    
+    }
+}
