9fd2b26dbc721977fc8b57d76eced6e021e328cb
SirBernhart <behornerl@gmail.com>
2021-04-18 18:01:47 -0300

    Criando script para o ataque melee dos inimigos

---
+++ ProjetoFinalLufer/Assets/Scripts/Enemies/MeleeEnemyAttack.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeEnemyAttack : MonoBehaviour
+{
+    [SerializeField] private Transform attackPoint;
+    [SerializeField] private LayerMask playerLayer;
+
+    public float sweepRange;
+
+    //Dano da arma equipada, caso a gente decida ter diferentes armas no jogo
+    public int weaponDamage;
+
+    public void Sweep()
+    {
+        //Detecção de inimigos
+        Collider []player = Physics.OverlapSphere(attackPoint.position, sweepRange, playerLayer);
+
+        //Dano
+        if(player.Length > 0)
+        {
+            player[0].GetComponent<HPManager>().TakeDamage(weaponDamage);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+        {
+            return;
+        }
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPoint.position, sweepRange);
+    }
+}
