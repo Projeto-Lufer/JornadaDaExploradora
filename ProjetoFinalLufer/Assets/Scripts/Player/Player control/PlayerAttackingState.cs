99425835fe1083bf29da86f2b563a35968471d28
SirBernhart <behornerl@gmail.com>
2021-04-24 20:00:53 -0300

    Adicionando tempo entre ataques e fazendo jogador ficar parado enquanto ataca

--- ProjetoFinalLufer/Assets/Scripts/Player/Player control/PlayerAttackingState.cs
+++ ProjetoFinalLufer/Assets/Scripts/Player/Player control/PlayerAttackingState.cs
@@ -4,16 +4,52 @@ using UnityEngine;
 
 public class PlayerAttackingState : ConcurrentState
 {
+    [Header("External references")]
     [SerializeField] private MeleeAttacks meleeAttack;
     [SerializeField] private Animator animator;
 
+    [Header("Gameplay tweeking fields")]
+    [SerializeField] private float[] timeBetweenAttacks;
+
+    // Internal variables
+    private List<WaitForSeconds> timeBetweenAttacksWFS;
+    private bool isAttacking;
+
+    private void Start()
+    {
+        timeBetweenAttacksWFS = new List<WaitForSeconds>();
+        foreach (float time in timeBetweenAttacks)
+        {
+            timeBetweenAttacksWFS.Add(new WaitForSeconds(time));
+        }
+    }
+
+    public override void Enter()
+    {
+        animator.SetTrigger("Attack");
+        meleeAttack.Sweep();
+        StartCoroutine(AttackDuration(timeBetweenAttacksWFS[0]));
+    }
+
     public override void HandleInput()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (!isAttacking)
         {
-            animator.SetTrigger("Attack");
-            //StartCoroutine(haltedTimerCoroutine(animationTimes["Attack"]));
-            meleeAttack.Sweep();
+            if (Input.GetButtonDown("Fire1"))
+            {
+                animator.SetTrigger("Attack");
+                meleeAttack.Sweep();
+                StartCoroutine(AttackDuration(timeBetweenAttacksWFS[0]));
+            }
         }
+    }
+
+    private IEnumerator AttackDuration(WaitForSeconds duration)
+    {
+        stateMachine.ChangeOtherStateMachineState(typeof(PlayerIdleState));
+        isAttacking = true;
+        yield return duration;
+        isAttacking = false;
+        stateMachine.ChangeState(typeof(PlayerNotActingState));
     }
 }
