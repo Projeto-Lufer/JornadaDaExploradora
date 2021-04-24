f3d200db35e6158ff514ca206c245f8857917e1e
SirBernhart <behornerl@gmail.com>
2021-04-24 18:18:11 -0300

    Criando estado de lifting para o jogador

---
+++ ProjetoFinalLufer/Assets/Scripts/Player/Player control/PlayerLiftingState.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerLiftingState : ConcurrentState
+{
+    [Header("Gameplay tweeking fields")]
+    [SerializeField] private float liftingDuration;
+
+    private WaitForSeconds liftingDurationWFS;
+
+    private void Start()
+    {
+        liftingDurationWFS = new WaitForSeconds(liftingDuration);
+    }
+
+    public override void Enter()
+    {
+        StartCoroutine(LiftingDurationTimer());
+    }
+
+    private IEnumerator LiftingDurationTimer()
+    {
+        stateMachine.ChangeOtherStateMachineState(typeof(PlayerIdleState));
+        yield return liftingDurationWFS;
+        stateMachine.ChangeState(typeof(PlayerHoldingState));
+    }
+
+    public override void Exit()
+    {
+        StopAllCoroutines();
+    }
+}
