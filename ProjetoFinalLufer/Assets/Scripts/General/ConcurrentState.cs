f9fcba384ec507cde80c6294714389404ad66262
SirBernhart <behornerl@gmail.com>
2021-04-24 18:15:09 -0300

    Criando estados de state machine concorrete

---
+++ ProjetoFinalLufer/Assets/Scripts/General/ConcurrentState.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConcurrentState : State
+{
+    protected ConcurrentStateMachine stateMachine;
+
+    private void OnValidate()
+    {
+        stateMachine = GetComponent<ConcurrentStateMachine>();
+    }
+}
