2e4c437e1af7440f8e7ef9107fb661ad2f461af1
SirBernhart <behornerl@gmail.com>
2021-04-24 18:15:48 -0300

    Criando script de estados simples (não concorrentes)

---
+++ ProjetoFinalLufer/Assets/Scripts/General/SimpleState.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleState : State
+{
+    protected StateMachine stateMachine;
+
+    private void OnValidate()
+    {
+        stateMachine = GetComponent<StateMachine>();
+    }
+}
