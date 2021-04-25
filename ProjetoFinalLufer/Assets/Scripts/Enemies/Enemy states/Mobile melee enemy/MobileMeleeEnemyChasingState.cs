93f0d9c2ebc54de06facfddda240a1ac97001cb7
SirBernhart <behornerl@gmail.com>
2021-04-25 17:26:59 -0300

    Criando estados de chasing e fighting do inimigo

---
+++ ProjetoFinalLufer/Assets/Scripts/Enemies/Enemy states/Mobile melee enemy/MobileMeleeEnemyChasingState.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MobileMeleeEnemyChasingState : SimpleAnimatableState
+{
+    [SerializeField] private AreaDetector chaseAreaDetector;
+    [SerializeField] private AreaDetector fightAreaDetector;
+    [SerializeField] private NavMeshAgent agent;
+
+
+    private void Start()
+    {
+        chaseAreaDetector.triggerExitCallback = ChangeToPatrolling;
+        fightAreaDetector.triggerEnterCallback = ChangeToFighting;
+    }
+
+    public override void Enter(GameObject target)
+    {
+        agent.isStopped = false;
+        base.PlayAnimationTrigger("Walking");
+        StartCoroutine(Chase(target.transform));
+    }
+
+    public override void Exit()
+    {
+        StopAllCoroutines();
+    }
+
+    IEnumerator Chase(Transform target)
+    {
+        while (true)
+        {
+            agent.destination = target.position;
+            yield return null;
+        }
+    }
+
+    // ==== State trasitions
+
+    private void ChangeToPatrolling(GameObject _)
+    {
+        stateMachine.ChangeState(typeof(MobileMeleeEnemyPatrollingState));
+    }
+
+    private void ChangeToFighting(GameObject target)
+    {
+        agent.isStopped = true;
+        stateMachine.ChangeState(typeof(MobileMeleeEnemyFightingState), target);
+    }
+
+}
---
+++ ProjetoFinalLufer/Assets/Scripts/Enemies/Enemy states/Mobile melee enemy/MobileMeleeEnemyFightingState.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobileMeleeEnemyFightingState : SimpleAnimatableState
+{
+    [Header("External references")]
+    [SerializeField] private AreaDetector fightAreaDetector;
+    [SerializeField] private Transform parentToRotate;
+    [SerializeField] private MeleeAttacks meleeAttack;
+
+    [Header("Gameplay tweeking fields")]
+    [SerializeField] private float attackPlayerDelay;
+    [SerializeField] private float attackDuration;
+    [SerializeField] private float turnSmoothTime = 0.1f;
+
+    private WaitForSeconds attackDurationWFS;
+    private float turnSmoothVelocity;
+
+    private void Start()
+    {
+        attackDurationWFS = new WaitForSeconds(attackDuration);
+        fightAreaDetector.triggerExitCallback = ChangeToChasing;
+    }
+
+    public override void Enter(GameObject target)
+    {
+        PlayAnimationTrigger("Fighting");
+        StartCoroutine(Fight(target.transform));
+    }
+
+    public override void Exit()
+    {
+        StopAllCoroutines();
+    }
+
+    IEnumerator Fight(Transform target)
+    {
+        while (true)
+        {
+            PlayAnimationTrigger("Fighting");
+            for (float i = 0; i < attackPlayerDelay; i += Time.deltaTime)
+            {
+                Vector3 direction = new Vector3(target.position.x - parentToRotate.position.x, 
+                                                transform.position.y,
+                                                target.position.z - parentToRotate.position.z);
+                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+                float angle = Mathf.SmoothDampAngle(parentToRotate.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+                parentToRotate.rotation = Quaternion.Euler(0f, angle, 0f);
+
+                //parentToRotate.LookAt(new Vector3(target.position.x, transform.position.y, target.position.z));
+                yield return null;
+            }
+            PlayAnimationTrigger("Attacking");
+            meleeAttack.Sweep();
+
+            yield return attackDurationWFS;
+        }
+    }
+
+    // ==== State trasitions
+
+    private void ChangeToChasing(GameObject target)
+    {
+        stateMachine.ChangeState(typeof(MobileMeleeEnemyChasingState), target);
+    }
+}
