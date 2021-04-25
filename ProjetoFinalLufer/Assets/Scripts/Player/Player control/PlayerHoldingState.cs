feff58d2bf7bc93218fa28a7283729354d3f3da4
SirBernhart <behornerl@gmail.com>
2021-04-25 13:40:36 -0300

    Removendo código desnecessário e adicionando marcador base.

--- ProjetoFinalLufer/Assets/Scripts/Player/Player control/PlayerHoldingState.cs
+++ ProjetoFinalLufer/Assets/Scripts/Player/Player control/PlayerHoldingState.cs
@@ -7,24 +7,17 @@ public class PlayerHoldingState : ConcurrentState
     [Header("External references")]
     [SerializeField] private ObjectManipulator objectManipulator;
 
-    // !!!! Relevant inherited attributes !!!!
-    // stateMachine -> from State
-    public override void Enter()
-    {
-        Debug.Log("Entrou no Holding");
-    }
-
     public override void HandleInput()
     { 
         if (Input.GetButtonDown("Interact"))
         {
             objectManipulator.ThrowObject();
-            stateMachine.ChangeState(typeof(PlayerNotActingState));
+            base.stateMachine.ChangeState(typeof(PlayerNotActingState));
         }
         else if (Input.GetButtonDown("Cancel"))
         {
             objectManipulator.ReleaseObject();
-            stateMachine.ChangeState(typeof(PlayerNotActingState));
+            base.stateMachine.ChangeState(typeof(PlayerNotActingState));
         }
     }
 }
