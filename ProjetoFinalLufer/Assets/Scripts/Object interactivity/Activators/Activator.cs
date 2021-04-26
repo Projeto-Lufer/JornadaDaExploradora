47060de9a36ef1b7d898610202b6f533d22c716a
SirBernhart <behornerl@gmail.com>
2021-04-26 09:48:29 -0300

    Atualizando activator para mudar de material e ser mais adequado para seus herdeiros

---
+++ ProjetoFinalLufer/Assets/Scripts/Object interactivity/Activators/Activator.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Activator : Interactive
+{
+    [SerializeField] protected Activateable objectToActivate;
+    [SerializeField] protected Material activatedMaterial;
+
+    protected Material deactivatedMaterial;
+    protected MeshRenderer meshRenderer;
+
+    private void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        deactivatedMaterial = meshRenderer.material;
+    }
+
+    public override void Interact()
+    {
+        meshRenderer.material = activatedMaterial;
+        objectToActivate.Activate();
+        objectToActivate.Activate(gameObject);
+    }
+}
