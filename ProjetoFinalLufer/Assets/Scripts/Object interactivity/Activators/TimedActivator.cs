e38b00921a9d6fa051115abeb76593ddb31428ad
SirBernhart <behornerl@gmail.com>
2021-05-01 16:42:52 -0300

    Criando script do botão que desativa depois de um tempo

---
+++ ProjetoFinalLufer/Assets/Scripts/Object interactivity/Activators/TimedActivator.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedActivator : Activator
+{
+    [SerializeField] private float activatedTime = 1;
+
+    private WaitForSeconds activatedTimeWFS;
+    private bool isActivated;
+
+    protected override void Start()
+    {
+        base.Start();
+        activatedTimeWFS = new WaitForSeconds(activatedTime);
+    }
+
+    public override void Interact()
+    {
+        if (!isActivated)
+        {
+            base.Interact();
+            isActivated = true;
+            StartCoroutine(DeactivationTimer());
+        }
+    }
+
+    private IEnumerator DeactivationTimer()
+    {
+        yield return activatedTimeWFS;
+        isActivated = false;
+        base.meshRenderer.material = base.deactivatedMaterial;
+        base.objectToActivate.Deactivate();
+    }
+}
