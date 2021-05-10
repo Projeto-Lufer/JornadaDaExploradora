3167e761669f31a16254c21d41153ece21deb85f
SirBernhart <behornerl@gmail.com>
2021-05-10 09:26:21 -0300

    Desenhando a área de efeito do ataque como seção circular

--- ProjetoFinalLufer/Assets/Scripts/General/MeleeAttacks.cs
+++ ProjetoFinalLufer/Assets/Scripts/General/MeleeAttacks.cs
@@ -7,6 +7,20 @@ public class MeleeAttacks : MonoBehaviour
     [SerializeField] private Transform attackPoint;
     [SerializeField] private LayerMask targetLayer;
 
+    [Header("Target area parameters")]
+    [SerializeField] private float fov = 90f;
+    [SerializeField] private float distance = 5f;
+
+    private float angle;
+    private Vector3 origin;
+    int rayCount = 50;
+    float angleIncrease;
+    Vector3[] vertices;
+    Vector2[] uv;
+    int[] triangles;
+    Mesh AOEMesh;
+    float startingAngle;
+
     public float sweepRange;
 
     public int weaponDamage;
@@ -23,12 +37,54 @@ public class MeleeAttacks : MonoBehaviour
 
     private void OnDrawGizmosSelected()
     {
-        if(attackPoint == null)
+        if (attackPoint == null)
         {
             return;
         }
 
+        float startingAngle = Mathf.Atan2(transform.forward.z, transform.forward.x) * Mathf.Rad2Deg;
+        if (startingAngle < 0) startingAngle += 360f;
+        
+        angle = startingAngle + fov/2;
+        origin = transform.position;
+        AOEMesh = new Mesh();
+        angleIncrease = fov / rayCount;
+        vertices = new Vector3[rayCount + 1 + 1];
+        uv = new Vector2[vertices.Length];
+        triangles = new int[rayCount * 3];
+
+        vertices[0] = origin;
+
+        int vertexIndex = 1;
+        int triangleIndex = 0;
+        for(int i = 0; i <= rayCount ; ++i)
+        {
+            float angleRad = angle * (Mathf.PI / 180f);
+            Vector3 vectorAngle = new Vector3(Mathf.Cos(angleRad), 0, Mathf.Sin(angleRad));
+
+            Vector3 vertex = origin + vectorAngle * distance;
+            vertices[vertexIndex] = vertex;
+
+            if (i > 0)
+            {
+                triangles[triangleIndex + 0] = 0;
+                triangles[triangleIndex + 1] = vertexIndex - 1;
+                triangles[triangleIndex + 2] = vertexIndex;
+
+                triangleIndex += 3;
+            }
+            ++vertexIndex;
+            angle -= angleIncrease;
+
+        }
+
+        AOEMesh.vertices = vertices;
+        AOEMesh.uv = uv;
+        AOEMesh.triangles = triangles;
+
+        AOEMesh.RecalculateNormals();
         Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(attackPoint.position, sweepRange);
+        Gizmos.DrawMesh(AOEMesh);
+
     }
 }
