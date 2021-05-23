07178d4f61d31645468d6dff653e5119fa15b4c7
SirBernhart <behornerl@gmail.com>
2021-05-23 10:54:24 -0300

    Porta interativa com chave

--- ProjetoFinalLufer/Assets/Scripts/Object interactivity/Interactives/InteractiveDoor.cs
+++ ProjetoFinalLufer/Assets/Scripts/Object interactivity/Interactives/InteractiveDoor.cs
@@ -4,8 +4,27 @@ using UnityEngine;
 
 public class InteractiveDoor : Interactive
 {
-    public override void Interact()
+    [SerializeField] private bool isLocked;
+    [SerializeField] private GameObject lockObject;
+
+    private void Start()
     {
-        Destroy(this.gameObject);
+        lockObject.SetActive(isLocked);
+    }
+
+    public override void Interact(GameObject player)
+    {
+        ObjectCollector collector = player.GetComponent<ObjectCollector>();
+
+        if (isLocked && collector.GetKeysPossessed() > 0)
+        {
+            isLocked = false;
+            collector.UseKey();
+            lockObject.SetActive(false);
+        }
+        else if(!isLocked)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
