969b77531c6bcc9932f7ffa7ad5b0654b23e98bb
SirBernhart <behornerl@gmail.com>
2021-05-30 11:58:33 -0300

    Criando estado de se esconder para o inimigo ranged

---
+++ ProjetoFinalLufer/Assets/Scripts/Enemies/Enemy states/Static ranged enemy/StaticRangedEnemyHidingState.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticRangedEnemyHidingState : SimpleAnimatableState
+{
+    [SerializeField] private RangeDetector hidingAreaDetector;
+    [SerializeField] private Transform enemyTransform;
+    [SerializeField] private Collider bodyCollider;
+
+    private Vector3 originalPosition;
+    private Vector3 hidingPosition;
+
+    private void Start()
+    {
+        originalPosition = enemyTransform.position;
+        hidingPosition = new Vector3(enemyTransform.position.x, enemyTransform.position.y * 0.2f, enemyTransform.position.z);
+    }
+
+    public override void Enter()
+    {
+        Collider[] hits = hidingAreaDetector.GetCollisionsInArea();
+        if (hits.Length > 0)
+        {
+            GameObject target = hits[0].gameObject;
+            Hide();
+            StartCoroutine(CheckIfStillInHidingRange());
+        }
+        else
+        {
+            ChangeToShooting();
+        }
+    }
+
+    public override void Exit()
+    {
+        Show();
+    }
+
+    private void Hide()
+    {
+        //PlayAnimationTrigger("Hide");
+        enemyTransform.position = hidingPosition;
+        bodyCollider.enabled = false;
+    }
+
+    private void Show()
+    {
+        enemyTransform.position = originalPosition;
+        bodyCollider.enabled = true;
+    }
+
+    IEnumerator CheckIfStillInHidingRange()
+    {
+        yield return null;
+        Collider[] hits = hidingAreaDetector.GetCollisionsInArea();
+        while (hits.Length > 0)
+        {
+            hits = hidingAreaDetector.GetCollisionsInArea();
+            yield return null;
+        }
+        ChangeToShooting();
+    }
+
+    private void ChangeToShooting()
+    {
+        stateMachine.ChangeState(typeof(StaticRangedEnemyShootingState));
+    }
+}
