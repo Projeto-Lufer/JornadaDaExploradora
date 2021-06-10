20762be93b3cec890dbb73d5e1c781a307ab7acd
SirBernhart <behornerl@gmail.com>
2021-06-10 00:59:17 -0300

    Adicionando método para mostrar e esconder o menu in game

--- ProjetoFinalLufer/Assets/Scripts/General/GameTransitionsManager.cs
+++ ProjetoFinalLufer/Assets/Scripts/General/GameTransitionsManager.cs
@@ -4,10 +4,25 @@ using UnityEngine.SceneManagement;
 public class GameTransitionsManager : MonoBehaviour
 {
     [SerializeField] private GameObject endGamePopup;
+    [SerializeField] private GameObject inGameMenu;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Escape"))
+        {
+            SetShowInGameMenu(!inGameMenu.activeInHierarchy);
+        }
+    }
 
     public void EndGame()
     {
+        SetShowInGameMenu(false);
         endGamePopup.SetActive(true);
+    }
+
+    public void SetShowInGameMenu(bool show)
+    {
+        inGameMenu.SetActive(show);
     }
 
     public void RestartFromLastCheckpoint()
