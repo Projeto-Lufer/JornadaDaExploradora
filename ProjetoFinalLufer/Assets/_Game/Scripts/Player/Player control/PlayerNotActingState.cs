a6bb62788d12c3ac7bc2e246621df27269b5386b
AlOzorio <39777406+AlOzorio@users.noreply.github.com>
2021-09-25 22:07:10 -0300

    Removendo pulo

--- ProjetoFinalLufer/Assets/_Game/Scripts/Player/Player control/PlayerNotActingState.cs
+++ ProjetoFinalLufer/Assets/_Game/Scripts/Player/Player control/PlayerNotActingState.cs
@@ -12,60 +12,58 @@ public class PlayerNotActingState : ConcurrentState
 
     public override void HandleInput()
     {
-        bool isGrounded = transform.parent.GetComponent<CharacterController>().isGrounded;
-        if(isGrounded)
-        {
-            if (stateMachine.inputManager.actionInteract.triggered)
-            {
-                Interactive interactive = interactiveIdentifier.PeekMostRelevantInteractive();
 
-                if(interactive != null)
+        if (stateMachine.inputManager.actionInteract.triggered)
+        {
+            Interactive interactive = interactiveIdentifier.PeekMostRelevantInteractive();
+
+            if(interactive != null)
+            {
+                System.Type interactiveType = interactive.GetType();
+                if (interactiveType == typeof(LiftableObject))
                 {
-                    System.Type interactiveType = interactive.GetType();
-                    if (interactiveType == typeof(LiftableObject))
-                    {
-                        interactiveIdentifier.PopMostrelevantInteractive();
-                        interactive.Interact();
-                        objectManipulator.LiftObject(interactive.gameObject);
-                        base.stateMachine.ChangeState(typeof(PlayerLiftingState));
-                    }
-                    else if(interactiveType.IsSubclassOf(typeof(Activator)) && interactiveType != typeof(GroundButton))
-                    {
-                        interactive.Interact();
-                    }
-                    else if (interactiveType == typeof(PushableObject))
-                    {
-                        interactiveIdentifier.PopMostrelevantInteractive();
-                        interactive.Interact();
-                        objectManipulator.GrabObject(interactive.gameObject);
-                        base.stateMachine.ChangeState(typeof(PlayerDraggingState));                 
-                    }
-                    else if (interactiveType == typeof(InteractiveDoor))
-                    {
-                        interactive.Interact(transform.parent.gameObject);
-                    }
-                    else if (interactiveType == typeof(ItemContainer))
-                    {
-                        interactive.Interact(transform.parent.gameObject);
-                    }
-                    else if(interactiveType == typeof(Guardian))
-                    {
-                        interactive.Interact(transform.parent.gameObject);
-                    }
+                    interactiveIdentifier.PopMostrelevantInteractive();
+                    interactive.Interact();
+                    objectManipulator.LiftObject(interactive.gameObject);
+                    base.stateMachine.ChangeState(typeof(PlayerLiftingState));
+                }
+                else if(interactiveType.IsSubclassOf(typeof(Activator)) && interactiveType != typeof(GroundButton))
+                {
+                    interactive.Interact();
+                }
+                else if (interactiveType == typeof(PushableObject))
+                {
+                    interactiveIdentifier.PopMostrelevantInteractive();
+                    interactive.Interact();
+                    objectManipulator.GrabObject(interactive.gameObject);
+                    base.stateMachine.ChangeState(typeof(PlayerDraggingState));                 
+                }
+                else if (interactiveType == typeof(InteractiveDoor))
+                {
+                    interactive.Interact(transform.parent.gameObject);
+                }
+                else if (interactiveType == typeof(ItemContainer))
+                {
+                    interactive.Interact(transform.parent.gameObject);
+                }
+                else if(interactiveType == typeof(Guardian))
+                {
+                    interactive.Interact(transform.parent.gameObject);
                 }
             }
-            else if (stateMachine.inputManager.actionAttack1.triggered)
-            {
-                stateMachine.ChangeState(typeof(PlayerAttackingState));
-            }
-            else if(stateMachine.inputManager.actionAttack2.triggered)
-            {
-                stateMachine.ChangeState(typeof(PlayerChargingState));
-            }
-            else if (stateMachine.inputManager.actionDefend.triggered && canDefend)
-            {
-                stateMachine.ChangeState(typeof(PlayerDefendingState));
-            }
         }
+        else if (stateMachine.inputManager.actionAttack1.triggered)
+        {
+            stateMachine.ChangeState(typeof(PlayerAttackingState));
+        }
+        else if(stateMachine.inputManager.actionAttack2.triggered)
+        {
+            stateMachine.ChangeState(typeof(PlayerChargingState));
+        }
+        else if (stateMachine.inputManager.actionDefend.triggered && canDefend)
+        {
+            stateMachine.ChangeState(typeof(PlayerDefendingState));
+        }
+
     }
 }
