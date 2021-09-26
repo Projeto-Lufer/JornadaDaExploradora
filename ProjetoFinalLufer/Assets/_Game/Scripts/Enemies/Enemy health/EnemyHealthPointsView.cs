600b28a8eaee083d336c1a28cb4f75d489dc9140
SirBernhart <behornerl@gmail.com>
2021-09-26 13:54:21 -0300

    Toca animação de stunned e VFX corretamente

--- ProjetoFinalLufer/Assets/_Game/Scripts/Enemies/Enemy health/EnemyHealthPointsView.cs
+++ ProjetoFinalLufer/Assets/_Game/Scripts/Enemies/Enemy health/EnemyHealthPointsView.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class EnemyHealthPointsView : MonoBehaviour
 {
     [SerializeField] private ParticleSystem hitVFX;
+    [SerializeField] private GameObject stunnedVFX;
 
     public void PlayDamageVisuals()
     {
         hitVFX.Play();
         //TODO: Play animation
+    }
+
+    public void SetStunnedVFXEnabled(bool enabled)
+    {
+        stunnedVFX.SetActive(enabled);
     }
 }
