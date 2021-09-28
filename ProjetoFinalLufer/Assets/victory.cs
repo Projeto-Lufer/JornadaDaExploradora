cf1bd571422807fc2b9abdb9a8e8b8fff576997f
Bernardo Gadelha <bernardompg@gmail.com>
2021-09-27 22:04:02 -0300

    Victory Screen + trigger atrás da porta final do boss

---
+++ ProjetoFinalLufer/Assets/victory.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class victory : MonoBehaviour
+{
+
+    public GameObject UIScreen;
+
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            UIScreen.SetActive(true);
+        }
+    }
+}
