a8e7cdd8a5f6fea136edc2bdb35d78fb4936f61f
SirBernhart <behornerl@gmail.com>
2021-10-23 10:40:22 -0300

    Cria script de objeto que gira quando leva um hit

---
+++ ProjetoFinalLufer/Assets/_Game/Scripts/Object interactivity/TurnableWhenHit.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnableWhenHit : HealthPoints
+{
+    [TextArea()]
+    [SerializeField] private string WARNING;
+
+    [SerializeField] private Transform objectToTurn;
+    [HideInInspector] public bool canTurn = false;
+
+    private Coroutine turnCoroutine = null;
+    private float maxTurnAmount = 90f;
+
+    public override void ReduceHealth(ComboElement attackStats)
+    {
+        LaserRedirector lr = base.GetComponentInChildren<LaserRedirector>();
+        if (lr != null)
+        {
+            Turn();
+            return;
+        }
+    }
+
+    public void Turn()
+    {
+        if (turnCoroutine == null)
+        {
+            turnCoroutine = StartCoroutine(TurnAnimation());
+        }
+    }
+
+    public IEnumerator TurnAnimation()
+    {
+        float totalRotation = 0;
+        float currTurnAmount = 0;
+
+        while (totalRotation < maxTurnAmount)
+        {
+            currTurnAmount += Time.deltaTime * 90;
+
+            if (totalRotation + currTurnAmount > maxTurnAmount)
+            {
+                currTurnAmount = maxTurnAmount - totalRotation;
+            }
+            objectToTurn.rotation *= Quaternion.AngleAxis(currTurnAmount, Vector3.up);
+
+            totalRotation += currTurnAmount;
+
+            yield return null;
+        }
+
+        turnCoroutine = null;
+    }
+}
