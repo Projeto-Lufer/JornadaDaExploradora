b1d4cd6333aeadc2b51a6bf89b4376e1eeed0654
Bernardo Hörner Lopes <bernardo.horner@tapps.com.br>
2021-11-16 22:11:41 -0300

    Remove imports desnecessários

--- ProjetoFinalLufer/Assets/_Game/Scripts/UI/InGameMenu.cs
+++ ProjetoFinalLufer/Assets/_Game/Scripts/UI/InGameMenu.cs
@@ -1,9 +1,7 @@
-using System;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.UI;
 
 public class InGameMenu : MonoBehaviour
 {
