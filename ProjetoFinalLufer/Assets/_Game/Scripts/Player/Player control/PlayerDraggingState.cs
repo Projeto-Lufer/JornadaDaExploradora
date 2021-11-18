20bca2e57d5aa987d49828421e77a5bde383f143
Bernardo Hörner Lopes <bernardo.horner@tapps.com.br>
2021-11-17 21:13:08 -0300

    Implementa animações de empurrar e puxar

--- ProjetoFinalLufer/Assets/_Game/Scripts/Player/Player control/PlayerDraggingState.cs
+++ ProjetoFinalLufer/Assets/_Game/Scripts/Player/Player control/PlayerDraggingState.cs
@@ -1,11 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class PlayerDraggingState : ConcurrentState
 {
     [Header("External references")]
     [SerializeField] private ObjectManipulator objectManipulator;
+    [SerializeField] private Animator animator;
+
+    public override void Enter()
+    {
+        animator.SetBool("Pushing", true);
+    }
+
+    public override void Exit()
+    {
+        animator.SetBool("Pushing", false);
+    }
 
     public override void HandleInput()
     {
--- ProjetoFinalLufer/Assets/_Game/Scripts/Player/Player control/PlayerMovingState.cs
+++ ProjetoFinalLufer/Assets/_Game/Scripts/Player/Player control/PlayerMovingState.cs
@@ -37,7 +37,10 @@ public class PlayerMovingState : ConcurrentState
 
     public override void Exit()
     {
+        Debug.Log("");
         animator.SetBool("Running", false);
+        animator.SetBool("PushingForward", false);
+        animator.SetBool("PushingBackwards", false);
     }
 
 
@@ -62,7 +65,7 @@ public class PlayerMovingState : ConcurrentState
 
         State otherSMState = stateMachine.GetOtherStateMachineCurrentState();
 
-        if (direction.magnitude >= 0.1f && otherSMState.GetType() != typeof(PlayerDraggingState) && pcs.canTurn == true)
+        if (direction.magnitude >= 0.1f && otherSMState.GetType() != typeof(PlayerDraggingState) && pcs.canTurn)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
             float angle = Mathf.SmoothDampAngle(playerRoot.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
@@ -85,6 +88,16 @@ public class PlayerMovingState : ConcurrentState
         }
         else if(otherSMState.GetType() == typeof(PlayerDraggingState))
         {
+            Vector2 directionInR2 = new Vector2(direction.x, direction.z);
+            Vector2 forwardInR2 = new Vector2(transform.forward.x, transform.forward.z);
+
+            bool movingForward = (Vector2.Dot(directionInR2, forwardInR2) > 0);
+            bool isMoving = directionInR2.magnitude > 0.1f;
+
+            animator.SetBool("PushingForward", movingForward && isMoving);
+            animator.SetBool("PushingBackwards", !movingForward && isMoving);
+            animator.SetFloat("MoveAmount", directionInR2.magnitude);
+
             currSpeed = speed / 2;
 
             if(Math.Abs(transform.forward.x) > Math.Abs(transform.forward.z))
