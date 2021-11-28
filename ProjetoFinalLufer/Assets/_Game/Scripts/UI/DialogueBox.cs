86c003e5b63fdfc3f0bf555ccd3f24967acbdaed
Nagib Moura Suaid <nagibmoura@gmail.com>
2021-11-28 15:49:29 -0300

    wip dialogue text display

--- ProjetoFinalLufer/Assets/_Game/Scripts/UI/DialogueBox.cs
+++ ProjetoFinalLufer/Assets/_Game/Scripts/UI/DialogueBox.cs
@@ -11,6 +11,8 @@ public class DialogueBox : MonoBehaviour
     [SerializeField] private GameObject backgroundWithName;
     [SerializeField] private TMP_Text characterName;
     [SerializeField] private TMP_Text dialogueText;
+    [SerializeField] private float textSpeed = 10f;
+    private bool isDisplaying = false;
 
     private Queue<string> sentences = new Queue<string>();
 
@@ -36,20 +38,45 @@ public class DialogueBox : MonoBehaviour
             backgroundWithName.SetActive(false);
             basicBackground.SetActive(true);
         }
+
         TryDisplayNextSentece();
     }
 
     public bool TryDisplayNextSentece()
     {
+        if(isDisplaying)
+        {
+            //ignore clicks while displaying
+            return true;
+        }
         if (sentences.Count == 0)
         {
             visuals.SetActive(false);
             return false;
         }
 
+        isDisplaying = true;
         string sentence = sentences.Dequeue();
         dialogueText.text = sentence;
 
         return true;
+    }
+
+    private IEnumerator DisplaySentence(string sentence)
+    {
+        dialogueText.text = "";
+        string currentText = "";
+        string finalText = sentence;
+
+        for (int i = 0; i < finalText.Length; i++)
+        {
+            currentText = finalText.Substring(0, i);
+            currentText += "<color=#00000000>" + finalText.Substring(i) + "</color>";
+            yield return new WaitForSeconds(1.0f/textSpeed);
+            dialogueText.text = currentText;
+        }
+        // helps avoiding accidently skipping dialogue
+        yield return new WaitForSeconds(0.5f);
+        isDisplaying = false;
     }
 }
